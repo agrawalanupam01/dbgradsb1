5addc7bc1b6d4a883ccd02f726de27c07f40216a
nilesh.devdas <nilesh.devdas@vinsys.com>
2020-08-14 10:55:43 +0530

    Bootstrap

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     var loginButton = document.getElementById("loginbtn");
     var password = document.getElementById("password");
-
     password.addEventListener("blur", function (event) {
         if (!validateEmpty(event.target))
             event.target.style.border = '2px solid red';
         else
             event.target.style.border = '1px solid black';
     });
-
     username.addEventListener("blur", function (event) {
         if (!validateEmpty(event.target))
             event.target.style.border = '2px solid red';
         else
             event.target.style.border = '1px solid black';
     });
-
     populateOptions();
     loginButton.addEventListener("click", function (event) {
-        console.log(event)
+        event.preventDefault();
         var errors = [];
         var isFormValid = false;
         var username = document.getElementById("username")
@@ -28,44 +25,33 @@ document.addEventListener("DOMContentLoaded", function () {
         isFormValid = validateEmpty(password)
         errors.push("Password is not valid")
         if (isFormValid) {
-            window.location.replace("success.html");
+            console.log("success")
+            window.location = "success.html";
         } else {
             errors.forEach((each) => {
                 document.getElementById("errors").append(each)
             });
             window.location.replace("error.html");
         }
+        return false;
     })
 });
 
 function populateOptions() {
-    console.log(1)
     var request = new XMLHttpRequest();
-    console.log("Created Request.............")
     request.open("GET", "https://run.mocky.io/v3/d8a99df5-bec4-43ce-8ed0-3165f6b5e97b", true);
-    console.log("Opened Connection ...................")
-
     request.onreadystatechange = function () {
-        // this function will get called when ever the state changes 2, 3, 4 [completion]
-        console.log("State Changed ................. ", request.readyState)
         if (request.readyState == 4 && request.status == 200) {
-            console.log("Got Response ................................", request.responseText)
             var domains_element = document.getElementById("domains");
             var result = request.responseText;
             var options_response = JSON.parse(result);
-            console.log("Converted The Response to JSON ", options_response);
             options_response.forEach((each) => {
-                domains_element.innerHTML = domains_element.innerHTML +
-                    "<option>" +
-                    each +
-                    "</option>"
+                domains_element.innerHTML = domains_element.innerHTML + "<option>" + each + "</option>"
             });
         }
     }
     request.send();
-    console.log("Sent Request ........................................")
 }
-
 
 function validateEmpty(elem) {
     if (elem.value == undefined || elem.value == null || elem.value == "") {
